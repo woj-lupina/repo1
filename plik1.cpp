dc8945861af60cd569d72694d9897b54b67a3a7c
mblasiak email = milblasiak@gmail.com <student166@pcl166.autolab.local>
2017-01-24 13:04:05 +0100

    Dodane pliki

---
+++ plik1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+ 
+void sortowanie_przez_wstawianie(int n, int *tab)
+{
+     int pom, j;
+     for(int i=1; i<n; i++)
+     {
+             //wstawienie elementu w odpowiednie miejsce
+             pom = tab[i]; //ten element bêdzie wstawiony w odpowiednie miejsce
+             j = i-1;
+ 
+             //przesuwanie elementów wiêkszych od pom
+             while(j>=0 && tab[j]>pom) 
+             {
+                        tab[j+1] = tab[j]; //przesuwanie elementów
+                        --j;
+             }
+             tab[j+1] = pom; //wstawienie pom w odpowiednie miejsce
+     }     
+}
---
+++ plik3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+class programming
+{
+   private:
+      int variable;
+ 
+   public:
+ 
+      void input_value()
+      {
+         cout << "In function input_value, Enter an integer\n";
+         cin >> variable;
+      }
+ 
+      void output_value()
+      {
+         cout << "Variable entered is ";
+         cout << variable << "\n";
+      }
+};
